dfba8f0d697d1db84c656d7490389ed03ed9a1ec
hruday95 <hruday.95@gmail.com>
2020-08-31 04:38:16 +0000

    push skeleton

---
+++ multi_bar_recog.py
@@ -0,0 +1,40 @@
+class multi_bar_find:
+
+    def __init__(self):
+        pass
+
+    def engulfing(self):
+        pass
+
+    def bullish_engulfing(self):
+        pass
+
+    def bearish_engulfing(self):
+        pass
+
+    def doji(self):
+        pass
+
+    def piercing_pattern(self):
+        pass
+
+    def dark_cloud_cover(self):
+        pass
+
+    def harami(self):
+        pass
+
+    def bullish_harami(self):
+        pass
+
+    def bearish_harami(self):
+        pass
+
+    def gaps(self):
+        pass
+
+    def morning_star(self):
+        pass
+
+    def evening_star(self):
+        pass
---
+++ single_bar_recog.py
@@ -0,0 +1,25 @@
+class simple_bar_find:
+    
+    def __init__(self,data):
+        pass
+
+    def marubozu(self):
+        pass
+
+    def spinning_top(self):
+        pass
+
+    def doji(self):
+        pass
+
+    def paper_umbrella(self):
+        pass
+
+    def hammer(self):
+        pass
+
+    def hanging_man(self):
+        pass
+
+    def shooting_start(self):
+        pass
